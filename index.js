ed9c44cec205214d60c59e1a9497da42f79de196
ramesh.annabhimoju <ramesh.annabhimoju@gmail.com>
2022-12-19 22:26:29 +0530

    "canges in cors"

--- index.js
+++ index.js
@@ -11,8 +11,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: "https://bestbuy-client.vercel.app/",
-    allowedHeaders: "Access-Control-Allow-Origin",
+    origin: "https://bestbuy-client.vercel.app",
   })
 );
 app.use("/uploads", express.static("uploads/"));
