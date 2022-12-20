46be82389517d0d5e238be29437fd80da6e55a7d
ramesh.annabhimoju <ramesh.annabhimoju@gmail.com>
2022-12-20 22:55:08 +0530

    "chnage in headers"

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ dotenv.config();
 const app = express();
 // app.use(cors({ origin: "https://bestbuy-client.vercel.app/" }));
 // app.use(cors({ origin: "https://localhost:3000/" }));
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", [
+    process.env.DEP_SERVER_URL,
+    process.env.DEV_SERVER_URL,
+  ]);
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.use("/uploads", express.static("public/uploads/"));
 const PORT = process.env.PORT || 8081;
