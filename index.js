5b330b20c834062fc23f4838f8e0c2d33e45a7c6
ramesh.annabhimoju <ramesh.annabhimoju@gmail.com>
2022-12-20 23:10:25 +0530

    "chnage in haeader"

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ const app = express();
 // app.use(cors({ origin: "https://bestbuy-client.vercel.app/" }));
 // app.use(cors({ origin: "https://localhost:3000/" }));
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", [
-    "https://bestbuy-client.vercel.app/",
-    "http://localhost:3000/",
-  ]);
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    // "https://bestbuy-client.vercel.app/"
+    "http://localhost:3000/"
+  );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
