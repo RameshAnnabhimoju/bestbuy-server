1e81537e761dd49d85ccfbff907c02ab251cc7f1
ramesh.annabhimoju <ramesh.annabhimoju@gmail.com>
2022-12-20 17:53:24 +0530

    "change in cors origin"

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ import cors from "cors";
 dotenv.config();
 const app = express();
 app.use(cors({ origin: "https://bestbuy-client.vercel.app/" }));
+// app.use(cors({ origin: "https://localhost:3000/" }));
+
 app.use("/uploads", express.static("public/uploads/"));
 const PORT = process.env.PORT || 8081;
 mongoose
